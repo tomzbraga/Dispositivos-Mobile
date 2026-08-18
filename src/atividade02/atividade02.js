5b5b4c0c1e1d6bb369b23554f039a8a31965a54b
unknown <2514290003@ACADEMICO.IESB.BR>
2026-08-18 10:32:51 -0300

    finaliza atividade 2

--- src/atividade02/atividade02.js
+++ src/atividade02/atividade02.js
@@ -7,3 +7,51 @@ const produto = {
 console.log(`Nome: ${produto.nome}, preço: ${produto.preco}, estoque: ${produto.estoque}`);
 
 const listaColegas = ["zé", "zezin", "zezão", "zezo"];
+
+const [a, b, c, d] = listaColegas;
+
+console.log(a, d);
+
+//BLOCO 2
+const cores = ["azul", "verde"];
+
+const novasCores = [...cores, "amarelo"];
+
+console.log(novasCores);
+
+function mediaNotas(...notas) {
+    const soma = notas.reduce((acc, nota) => acc + nota, 0);
+    return soma / notas.length;
+}
+
+console.log(mediaNotas(2, 2))
+
+//BLOCO 3
+
+const precos = [50, 120, 35, 200]
+
+const precoFiltrado = precos.filter((preco) => preco >= 100)
+const precoDesconto = precos.map((preco) => preco * 0.9);
+const precoSoma     = precos.reduce((total, preco) => total + preco);
+
+console.log(precoFiltrado);
+console.log(`Preços com desconto: ${precoDesconto}`);
+console.log(`A soma de todos os preços: ${precoSoma}`);
+
+const alunos = [
+    {
+        id: 1, nome: "Ana",
+        id: 2, nome: "José",
+        id: 3, nome: "Fulano"
+    }
+];
+
+const filtraAlunos = alunos.find((a) => a.id === 3 )
+
+console.log(filtraAlunos.nome);
+
+// BLOCO 4
+
+let nome = "Léo";
+console.log(`Olá ${nome}`);
+
---
+++ src/atividade02/importaMatematica.js
@@ -0,0 +1,3 @@
+import { somar } from "./matematica.js";
+
+console.log(somar(3, 3))
