b833855d11ef6dedfffde243426284ace478853c
unknown <2514290003@ACADEMICO.IESB.BR>
2026-08-18 09:05:40 -0300

    inicia atividade 2

---
+++ src/atividade02/atividade02.js
@@ -0,0 +1,9 @@
+const produto = {
+    nome: "arroz",
+    preco: 10,
+    estoque: 100
+}
+
+console.log(`Nome: ${produto.nome}, preço: ${produto.preco}, estoque: ${produto.estoque}`);
+
+const listaColegas = ["zé", "zezin", "zezão", "zezo"];
